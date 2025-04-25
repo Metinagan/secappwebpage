a3a1696f37631efd7fb3f8247d91db238deaa475
Metin Agan <130462728+Metinagan@users.noreply.github.com>
2025-04-25 11:01:15 +0300

    added admin panel task sorting

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "f9f54dd2a9ee13053dd439fcd44cc73d",
+"flutter_bootstrap.js": "93209a2de8b10ebce98d038ecb92ecd0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "014566692439243bcd652b632c6abe65",
 "/": "014566692439243bcd652b632c6abe65",
-"main.dart.js": "9c0e90ecaaa309115472a98cf9c434eb",
+"main.dart.js": "a26bc1952a281cb837562a679165543e",
 "manifest.json": "0974660f18eb4c302b68150a4c3abcc8",
 "version.json": "4cff2a971fbfa124e3c4ae1f518963c3"};
 // The application shell files that are downloaded before a service worker can
